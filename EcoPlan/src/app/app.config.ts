26891debb10821f933e8b42c32a82ab7418b51cb
fabocv <fabianleandro.cv@gmail.com>
2026-02-05 17:34:20 -0300

    configuraciones para gh-actions

--- EcoPlan/src/app/app.config.ts
+++ EcoPlan/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -9,7 +9,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes, withHashLocation()), provideClientHydration(withEventReplay()),
     QueryImpactAnalyzer, provideHttpClient()
   ]
 };
